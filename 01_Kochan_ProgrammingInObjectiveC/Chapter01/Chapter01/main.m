64f27fe81525b7f8274920518084c8abdef162e2
Sergey Philippov <Quadratte@gmail.com>
2026-03-08 19:59:17 +0300

    Initial commit: project structure and Chapter 1 from Kochan

---
+++ 01_Kochan_ProgrammingInObjectiveC/Chapter01/Chapter01/main.m
@@ -0,0 +1,10 @@
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+
+        NSLog(@"Hello, World!");
+        NSLog(@"This is my first ObjC lesson");
+    }
+    return 0;
+}
